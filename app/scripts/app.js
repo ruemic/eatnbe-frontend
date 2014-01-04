ec691b1442cc7a7ed36c51c97a2115f5b9fb6a86
ruemic <rumicdesign@gmail.com>
2014-01-04 03:20:24 -0800

    use active tab lib for dashboard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,23 +4,52 @@ angular.module('eatnbeApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.router'
+
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    // Default to index state
+    $urlRouterProvider.otherwise("index");
+
+    // Define routes
+    $stateProvider
+      .state('index', {
+        url: "/index",
+        templateUrl: "views/main.html",
         controller: 'MainCtrl'
       })
-      .when('/myroute', {
-        templateUrl: 'views/myroute.html',
-        controller: 'MyrouteCtrl'
-      })
-      .when('/results', {
-        templateUrl: 'views/results.html',
+
+      .state('results', {
+        url: "/results",
+        templateUrl: "views/results.html",
         controller: 'ResultsCtrl'
       })
-      .otherwise({
-        redirectTo: '/'
-      });
+      .state('results.more', {
+        url: "/results-more",
+        templateUrl: "views/results.more.html"
+      })
+
+      .state('dashboard', {
+        url: "/dashboard",
+        templateUrl: "views/dashboard.html",
+        controller: 'DashboardCtrl'
+      })
+      .state('dashboard.profile', {
+        url: "/profile",
+        templateUrl: "views/dashboard.profile.html"
+      })
+      .state('dashboard.events', {
+        url: "/events",
+        templateUrl: "views/dashboard.events.html"
+      })
+      .state('dashboard.inbox', {
+        url: "/inbox",
+        templateUrl: "views/dashboard.inbox.html"
+      })
+     .state('dashboard.billing', {
+        url: "/billing",
+        templateUrl: "views/dashboard.billing.html"
+      })
   });
---
+++ app/scripts/controllers/dashboard.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('eatnbeApp')
+  .controller('DashboardCtrl', function ($scope) {
+
+  });
