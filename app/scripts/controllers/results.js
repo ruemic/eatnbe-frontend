41f059bbf49a131085120a4f967f2c253597d101
ruemic <rumicdesign@gmail.com>
2014-01-03 18:24:57 -0800

    remane js refs, add old map to resultsCtrl

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -5,4 +5,35 @@ angular.module('eatnbeApp')
 
     $scope.events = eventService.query();
 
+    var map = new GMaps({
+      lat: 36.9575335,
+      lng: -122.0400129,
+      div: '#map_canvas'
+    });
+
+    map.drawOverlay({
+      lat: 36.9575335,
+      lng: -122.0400129,
+      content: '<div class="current-location"><i class="fa fa-user"></i></div>'
+    });
+
+    map.addMarker({
+      lat: 36.9575335 + .003,
+      lng: -122.0400129 + .006,
+      title: 'Fried Chilly Squid',
+      details: { price: 22 },
+      infoWindow: {
+      content: '<div class="map-tip"><h4 class="text-center">Fried Chilly Squid</h4><img class="text-center" src="http://placehold.it/500x312" class="expand" alt=""></div>'
+      }
+    });
+    map.addMarker({
+      lat: 36.9575335 - .001,
+      lng: -122.0400129 - .006,
+      title: 'Fried Chilly Squid',
+      details: { price: 22 },
+      infoWindow: {
+      content: '<div class="map-tip"><h4 class="text-center">Fried Chilly Squid</h4><img class="text-center" src="http://placehold.it/500x312" class="expand" alt=""></div>'
+      }
+    });
+
   });
