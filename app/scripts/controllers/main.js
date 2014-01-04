67799ffc00ce49b0f27bf08127d9ce5ea022b2bd
ruemic <rumicdesign@gmail.com>
2014-01-03 21:03:24 -0800

    cleanup host toggle on homepage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,12 +3,27 @@
 angular.module('eatnbeApp')
   .controller('MainCtrl', function ($scope) {
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  $('chkbx').attr("checked");
+
+  $scope.toggleHostView = function() {
+    $('#how-it-works').toggleClass('hosting');
+  };
 
 
+  // Facebook button
 
+  var fbshares, fbsite;
+
+  fbsite = "EatnBe";
+
+  $.getJSON("http://graph.facebook.com/" + fbsite + "?callback=?", function(json) {
+    return $(".likes").append(json.likes);
   });
+
+  fbshares = "http://www.eatnbe.co/teaser-eatnbe/";
+
+  $.getJSON("http://graph.facebook.com/" + fbshares + "?callback=?", function(json) {
+    return $(".shares").append(json.shares || 0 );
+  });
+
+});
