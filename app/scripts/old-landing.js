8f4dbb56b2e9a1c516b9817c4e9e4c8c15013995
ruemic <rumicdesign@gmail.com>
2014-01-03 18:14:06 -0800

    delete files

---
+++ app/scripts/old-landing.js
@@ -0,0 +1,54 @@
+
+
+// // Use history.js to persist Host/Guest toggle state
+// History.Adapter.bind(window,'statechange',function(){
+//   if ( History.getState().title == "Host"  ) {
+//     $('body').addClass('hosting');
+//   }
+//   else if ( History.getState().title == "Guest"  ) {
+//     $('body').removeClass('hosting');
+//   }
+// });
+
+// $( document ).ready(function() {
+//   if ( History.getState().title == "Host" ) {
+//     $('#chkbx').prop('checked', false);
+//     $('body').addClass('hosting');
+
+//   } else {
+//     $('#chkbx').prop('checked', true);
+//   }
+// });
+
+// $("#chkbx").on("click", function() {
+//   if ($(this).is(":checked") ) {
+//     History.pushState({}, "Guest", "?guest");
+//   } else {
+//     History.pushState({}, "Host", "?host");
+//   }
+// });
+
+
+
+
+// Facebook button
+
+(function() {
+  var fbshares, fbsite;
+
+  fbsite = "EatnBe";
+
+  $.getJSON("http://graph.facebook.com/" + fbsite + "?callback=?", function(json) {
+    return $(".likes").append(json.likes);
+  });
+
+  fbshares = "http://www.eatnbe.co/teaser-eatnbe/";
+
+  $.getJSON("http://graph.facebook.com/" + fbshares + "?callback=?", function(json) {
+    return $(".shares").append(json.shares || 0 );
+  });
+
+}).call(this);
+
+
+
