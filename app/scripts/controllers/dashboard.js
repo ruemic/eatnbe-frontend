b08c6b990e8e33bf0a53e42e675e4183629e0126
ruemic <rumicdesign@gmail.com>
2014-01-06 20:46:38 -0800

    event dash details

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -3,4 +3,17 @@
 angular.module('eatnbeApp')
   .controller('DashboardCtrl', function ($scope) {
 
+    $scope.showMore = function(obj, $event) {
+      $(event.target).next('.more').fadeToggle();
+
+    }
+
+    $scope.messageModal = function() {
+      $('#send-a-message').foundation('reveal', 'open');
+    };
+
+    $scope.closeModal = function() {
+      $('#send-a-message').foundation('reveal', 'close');
+    };
+
   });
